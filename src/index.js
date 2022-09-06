858f4e5f13a69ff37f23e9816bbe5bf9dce661b7
rebaz36 <rebaz415@gmail.com>
2022-09-06 22:40:51 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
-import Icon from './icon.png';
+import Icon from './icon.jpg';
 
 function component() {
-  const element = document.createElement('div');
+  // const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  // // Lodash, now imported by this script
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.classList.add('hello');
 
   // Add the image to our existing div.
   const myIcon = new Image();
   myIcon.src = Icon;
+  myIcon.classList.add('icon');
 
-  element.appendChild(myIcon);
+  const elem = document.getElementById('logo');
+  elem.classList.add('logo');
+  elem.appendChild(myIcon);
 
-  return element;
+  // return element;
 }
 
 document.body.appendChild(component());
